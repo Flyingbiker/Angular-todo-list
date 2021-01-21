01841534d3a9a3a8a737bed54bf64e9129013fb7
aymeric <schoebel.aymeric@gmail.com>
2021-01-21 10:10:53 +0100

    methode addItem avec le as et le instanceOf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,22 @@ export class AppComponent {
     'item 4',
     'item 5',
   ]
+  
+  //: ici permet de déterminer le type de retour de la fonction 
+  //le void permet ici de dire qu'on n'attend pas de retour
+  public addItem($event: KeyboardEvent): void {
+    if ($event.code === 'Enter'){
+      // const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+      //le as permet de faire croire à la constante que c'est un HTMLInputElement
+
+      const $input : EventTarget |null = $event.target;
+
+      if ($input instanceof HTMLInputElement) {
+        console.log($input.value);  
+        $input.value;      
+      }
+      
+      console.log('Enter pressed');
+    }
+  }
 }
