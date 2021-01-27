f94bf7314dc5d4a792b09ea5393f03d265792049
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-27 15:38:56 +0100

    modif avec l'output sur les checkbox

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,5 +1,4 @@
-import { TodoItem } from './../todo-item.d';
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -19,16 +18,30 @@ export class CheckboxComponent implements OnInit {
   public label : string|null = null;
   //ajout de la valeur null pour initialiser l'attribut (sinon il y a une erreur)
   //c'est du au typage strict de TS
-  @Input()
-  public checked  = false;
+
+  // @Input()
+  // public checked  = false;
   //pas besoin de mettre le type boolean car on le déclare à false après
 
   //pour faire remonter le fait de cocher la checkbox vers le parent
-  
+  @Output()
+  public checkedChange = new EventEmitter();
+  //le composition du nom est le nom de l'attribut + Change (nomenclature Angular)
 
   constructor() { 
     this.id = 'app-checkbox-' + CheckboxComponent.counter;
     CheckboxComponent.incrementCounter();
+   }
+
+   //getter et setter de checked ligne 23   
+   private internalChecked = false;
+   public get checked():boolean {
+    return this.internalChecked;
+   }
+   @Input() // @Input pour le setter car on veut récupérer la valeur de l'attribut dans la balise
+   public set checked(val : boolean) {
+    this.internalChecked = val;
+    this.checkedChange.emit(this.internalChecked);
    }
 
   //exmple avec une méthode statique
