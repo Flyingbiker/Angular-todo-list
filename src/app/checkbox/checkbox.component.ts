09a311f99f032d0b3a35ba93e60e2fb4a513fd2f
Flyingbiker <schoebel.aymeric@gmail.com>
2021-01-27 10:31:48 +0100

    méthode static appliqué pour compter le nombre de fois où le composant est appelé

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckboxComponent implements OnInit {
 
-  constructor() { }
+  //l'attribut static est un attribut commun de la classe
+  //et n'appartient pas aux instances de la classe
+  private static counter = 0;
+
+  public id : string = 'app-checkbox-'+0;
+
+
+  constructor() { 
+    this.id = 'app-checkbox-' + CheckboxComponent.counter;
+    CheckboxComponent.counter++;
+   }
 
   ngOnInit(): void {
   }
